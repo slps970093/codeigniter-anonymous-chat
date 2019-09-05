cc9360f079b300d4d77b84625d78528f5f120cf3
Yu-Hsien Chou <slps970093@gmail.com>
2019-09-05 18:14:48 +0800

    add package

---
+++ application/controllers/Workerman-Server/WebSocketController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * WebSocket Server
+ * Class WebSocketController
+ *
+ * @another 小周
+ */
+
+class WebSocketController extends  CI_Controller
+{
+
+    public function __construct() {
+        if (!is_cli()) {
+            show_404();
+            die();
+        }
+        parent::__construct();
+    }
+
+    public function start() {
+
+    }
+}
